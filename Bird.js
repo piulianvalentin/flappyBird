93e653494804371948c720702bbba60db554c822
Purcelea Iulian Valentin <p.iulian.valentin@outlook.com>
2018-02-23 01:34:55 +0200

    Add Start, Score and Bird rotation

--- Bird.js
+++ Bird.js
@@ -5,17 +5,50 @@ class Bird {
 		this.graphics = graphics;
 		this.x = 80;
 		this.y = 260;
-		this.speedY = -6;
+		this.speedY = -2;
 		this.crash = false;
+		this.angleRotation;
 	}
 
 	show() {
 		
 		push();
-		imageMode(CENTER); 
-		image(this.graphics, this.x, this.y, 40, 40 );
+		imageMode(CENTER);
+		translate(this.x, this.y);
+	
+//      Bird rotation by speed
+		switch( true ) {
+			case ( this.speedY > 3 ):
+				this.angleRotation = -30;
+					break;
+			case ( this.speedY > 2.5 ):
+				this.angleRotation = -23;
+					break;
+			case ( this.speedY > 2 ):
+				this.angleRotation = -15;
+					break;
+			case ( this.speedY > 1.5 ):
+				this.angleRotation = 7;
+					break;
+			case ( this.speedY > -3 ):
+				this.angleRotation = 0;
+					break;
+			case ( this.speedY > -5 ):
+				this.angleRotation = 7;
+					break;
+			case ( this.speedY > -5.5 ):
+				this.angleRotation = 15;
+					break;
+			case ( this.speedY > -6 ):
+				this.angleRotation = 22;
+					break;
+			case ( this.speedY > -6.5 ):
+				this.angleRotation = 30;
+					break; 		
+		}		
+		rotate(this.angleRotation);
+		image(this.graphics, 0, 0, 40, 40 );
 		pop();
-		console.log(this.x, this.y);
 
 	}
 
@@ -23,11 +56,13 @@ class Bird {
 		
 		if( this.speedY > -8  )
 			this.speedY -= 0.5;
+
 //		Stop the bird when it hits top
 		if( this.y <= 0 ) {
 			this.y = 0;	
 			this.speedY = -0.5;			
 		}
+
 		this.y -= this.speedY;		
 	}
 
@@ -40,17 +75,19 @@ class Bird {
 		
 		
 //		Ground collide
-		if( this.y + 20 >= height * 0.83 )
+		if( this.y + 20 >= height * 0.845 ) {
+			( this.crash ? '' : sound.hit.play() );
 			this.crash = true;
+		}
 
-// 		Pipes collide assuming the bird it's circle shaped
+// 		Pipes collide assuming the bird it's circle shaped 
 		if( pipeX != undefined ) {
 
 //			Bird near pipes
 			if( this.x + 20 >= pipeX && this.x - 20 <= pipeX + 66 ) {
 
 //				Collide with the left side of the pipes and right corner
-				if( this.x < pipeX || this.x >= pipeX + 66) {
+				if( this.x < pipeX || this.x >= pipeX + 66 ) {
 
 //					Create collide points every 2px 
 					var cnt = 0;
@@ -58,26 +95,34 @@ class Bird {
 
 //  					Heads of the pipes are 3px longer and have a height of 20px 					
 						cnt++;
-						if(cnt == 10)
+						if( cnt == 10 )
 							pipeX += 3;
 //						Collide with the lower pipe
-						if(dist(this.x, this.y, pipeX, point) <= 19) {
+						if( dist( this.x, this.y, pipeX, point ) <= 14) {
 							this.crash = true;
+							sound.hit.play();
 						}
 //						Collide with the upper pipe
-						if(dist(this.x, this.y, pipeX, pipeY - 150 - (point - pipeY)) <= 18) {
+						if( dist( this.x, this.y, pipeX, pipeY - 150 - ( point - pipeY ) ) <= 14 ) {
 							this.crash = true;
+							sound.hit.play();
 						}
 					}
-//					Collide right corner of the pipe
-					if(dist(this.x, this.y, pipeX + 66, pipeY) <= 21) {
+//					Collide right corner of the pipe 
+					if( dist( this.x, this.y, pipeX + 66, pipeY ) <= 18 ) {
 							this.crash = true;
+							sound.hit.play();
+						}
+					if(dist(this.x, this.y, pipeX + 66, pipeY - 150) <= 13 ) {
+							this.crash = true;
+							sound.hit.play();
 						}
 				}
 //				Bird is between the pipes
 				else {
-					if(this.y + 25 >= pipeY || this.y - 16 <= pipeY - 150) {
+					if( this.y + 19 >= pipeY || this.y - 12 <= pipeY - 150 ) {
 						this.crash = true;
+						sound.hit.play();
 					}
 				}				
 			}
--- sketch.js
+++ sketch.js
@@ -5,14 +5,31 @@ var pipeImage;
 var bird, ground, pipes = [];
 var counter = 0;
 var start = 110;
+var scoreImages = [];
+var score;
+var sound = {};
+var soundScoreTrigger = 0;
+var gameStart = false;
+var flappyBirdTextImage;
 
 function preload() {
 	
 	frameRate(60); 
+
+//	Sound
+	sound.hit = loadSound('sound/sfx_hit.wav');
+	sound.wing = loadSound('sound/sfx_wing.wav');
+	sound.point = loadSound('sound/sfx_point.wav');
+
+//	Images
+	for (var i = 0; i < 10; i++) {
+		scoreImages.push(loadImage("images/score/" + i + ".png"));
+	}
 	birdImage = loadImage("images/yellowBird.png");
 	backgroundImage = loadImage("images/background.png")
 	groundImage = loadImage("images/ground.png");
 	pipeImage = loadImage("images/pipe.png");
+	flappyBirdTextImage = loadImage("images/flappyBirdText.png");
 }
 
 function setup() {
@@ -21,17 +38,24 @@ function setup() {
 	angleMode(DEGREES);
 	bird = new Bird(birdImage);
 	ground = new Ground(groundImage);
-
-
+	image(backgroundImage, 0, 0, width + 5, height);
+	bird.show();
+	ground.show();
+	image(flappyBirdTextImage, 75, 100, 250, 100);
+	bird.crash = true;
+	textSize(30);
+	fill(255);
+	text('Press SPACE to play', 70, 350);
 }
 
 function draw() {
 	
-	if(!bird.collide()) {
+// 	Game playing if there is no collision
+	if( !bird.collide() ) {
 		
 		image(backgroundImage, 0, 0, width + 5, height);
 		counter++;
-		
+
 		if(counter > start) {
 			for(var i = 0; i < pipes.length; i++) {
 				pipes[i].show();
@@ -42,20 +66,49 @@ function draw() {
 
 		if(counter % 100  == 0) {
 			pipes.push(new Pipes(pipeImage, width, random(250, 450)));
+//			Keep a maximum of three pipes
 			if(pipes.length >= 4) {
 				pipes.shift(); 
 			}
 	}
 	ground.show();
 	ground.move();
+	bird.show();   
 	bird.move();
-	bird.show();	  
-	}		
+
+	//	For keeping the score. Counter will be divided by 100 after we substract the start
+		score = parseInt( ( counter - start - 70 ) / 100 );
+		
+		if( score < 0)
+			score = 0;
+
+		if ( score != soundScoreTrigger ) {
+			soundScoreTrigger++;
+			sound.point.play();
+		}
+
+		score = score + "";
+		for( var i = 0; i < score.length; i++ ) {
+			image( scoreImages[ score[i] ], 200 + i * 25, 80, 25, 40 ); 
+		}	  
+	}
+
+
 }
 
 function keyPressed() {
+	
+	if( !gameStart ) {
+		gameStart = true;
+		bird.crash = false;
+	}
+
 	if(keyCode  === 32)
 		bird.jump();
+
+	if( !bird.collide() ) {
+		sound.wing.play();
+	}
 }
 
 
